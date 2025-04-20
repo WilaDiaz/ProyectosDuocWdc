46672fa19ff974a7af3ee85334b20bb1dbf649e8
Wila Diaz <wila.diaz@duocuc.cl>
2025-04-20 17:50:35 -0400

    Add files via upload

---
+++ Exp6_S6_Wilangely_Diaz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exp6_s6_wilangely_diaz;
+
+/**
+ *
+ * @author wdiaz
+ */
+public class Exp6_S6_Wilangely_Diaz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
